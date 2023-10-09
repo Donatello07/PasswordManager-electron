9c37472aebff5fbfa35c807c9f83eb5080b68374
Ramdhany Kaisupy <ramdhany.kaisupy@gmail.com>
2023-10-09 18:13:21 +0700

    update

--- renderer.js
+++ renderer.js
@@ -1,7 +1,9 @@
 $('#form-new-item').click(function () {
     $('#new-item-container').addClass('show')
-
     $('#new-item-container').removeClass('hide')
+})
+
+function apiCall() {
     // $.ajax({
     //     type: 'GET',
     //     url: "http://192.168.1.10:3000/password?password=password2",
@@ -15,68 +17,47 @@ $('#form-new-item').click(function () {
     //         alert(error)
     //     }
     // });
-})
+}
 
-function addItems(jumlahitem){
+function addItems(jumlahitem) {
     for (let index = 0; index < jumlahitem; index++) {
-        let element= `<div class="items-container">
-        <input type="checkbox" name="items" id="1">
-        <img src="./assets/images/contoh%20icon.ico" class="" id="image1" width="20" height="20">
+        let element = `<div class="items-container">
+        <div class="item-logo">
+                <img src="./assets/icons/favicon.ico" class="item-image-logo" width="20" height="20">
+              </div>
         <div class="item-named" >
-          <span>Item ${index+3}</span>
+          <span class="passwordtitle">${index + 2}. Google</span>
+          <span class="passwordemail">${index + 2}. ramdhany.kaisupy@gmail.com</span>
         </div>
+        <div class="item-option">
+                <img src="./assets/icons/ellipsis-solid.svg" width="15" height="15">
+              </div>
       </div>`;
-      $('#main-item-container').append(element);
-        
+        $('#main-item-container').append(element);
+
     }
 }
-$('#icon-close-new-item').click(function(){
+$('#icon-close-new-item').click(function () {
     $('#new-item-container').removeClass('show')
     $('#new-item-container').addClass('hide')
-    
+
 })
+
+
+
 $(document).ready(function () {
     addItems(10)
 
+    $('.items-container').each(function () {
+        
+        $(this).hover(function () {
+            var $item = $(this);
+            $('.item-option img').each(function(){
+                $(this).hide();
+            })
+            $item.find('.item-option img').each(function () {
+                $(this).show()
+            })
+        })
+    })
 });
-
-// const information = document.getElementById('form-new-item')
-// const electron = require('electron');
-// const net = electron.remote.net;
-
-// information.addEventListener('click', () => {
-//     alert("you just click new item")
-//     ApiCall();
-// })
-
-// function ApiCall() {
-//     const request = net.request({
-//         method: 'GET',
-//         protocol: 'http:',
-//         hostname: 'http://192.168.1.10:3000/password?passwordname=password2',
-//         path: '/get',
-//         redirect: 'follow'
-//     });
-//     request.on('response', (response) => {
-//         console.log(`STATUS: ${response.statusCode}`);
-//         console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-
-//         response.on('data', (chunk) => {
-//             console.log(`BODY: ${chunk}`)
-//         });
-//     });
-//     request.on('finish', () => {
-//         console.log('Request is Finished')
-//     });
-//     request.on('abort', () => {
-//         console.log('Request is Aborted')
-//     });
-//     request.on('error', (error) => {
-//         console.log(`ERROR: ${JSON.stringify(error)}`)
-//     });
-//     request.on('close', (error) => {
-//         console.log('Last Transaction has occurred')
-//     });
-//     request.setHeader('Content-Type', 'application/json');
-//     request.end()
-// }
