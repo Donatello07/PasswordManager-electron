2d538a3fe8e2570ae6089f904e7566e3f1f61e91
Ramdhany Kaisupy <ramdhany.kaisupy@gmail.com>
2023-10-11 16:35:54 +0700

    add icons

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
+const proxy = require("node-global-proxy").default;
 
 const createWindow = () => {
+    proxy.setConfig({
+        http: "http://fady.kaisupy:12345@10.88.88.15:1080",
+        https: "https://fady.kaisupy:12345@10.88.88.15:1080",
+      });
+    proxy.start();
+
     const win = new BrowserWindow({
         width: 800,
         height: 600,
--- renderer.js
+++ renderer.js
@@ -19,45 +19,58 @@ function apiCall() {
     // });
 }
 
-function addItems(jumlahitem) {
-    for (let index = 0; index < jumlahitem; index++) {
-        let element = `<div class="items-container">
-        <div class="item-logo">
-                <img src="./assets/icons/favicon.ico" class="item-image-logo" width="20" height="20">
-              </div>
-        <div class="item-named" >
-          <span class="passwordtitle">${index + 2}. Google</span>
-          <span class="passwordemail">${index + 2}. ramdhany.kaisupy@gmail.com</span>
-        </div>
-        <div class="item-option">
-                <img src="./assets/icons/ellipsis-solid.svg" width="15" height="15">
-              </div>
-      </div>`;
-        $('#main-item-container').append(element);
+function GetAllPasswordItem(){
+    const URl="http://localhost:3000/password";
+    $.ajax({
+        type: 'GET',
+        url: URl,
+        dataType: 'json',
+        success: function (result) {
+            console.log(JSON.stringify(result))
+            addItems(result._data)
+            $('.items-container').each(function () {
+                $(this).hover(function () {
+                    var $item = $(this);
+                    $('.item-option img').each(function(){
+                        $(this).hide();
+                    })
+                    $item.find('.item-option img').each(function () {
+                        $(this).show()
+                    })
+                })
+            })
+        },
+        error:function(error){
+            alert(error)
+        }
+    });
+}
 
+function addItems(item) {
+    for (let i = 0; i < item.length; i++) {
+        let data = item[i];
+        let element = `<div class="items-container">
+                            <div class="item-logo">
+                                <img src="${data.iconurl}" class="item-image-logo" width="20" height="20">
+                            </div>
+                            <div class="item-named" >
+                                <span class="passwordtitle">${data.passwordname}</span>
+                                <span class="passwordemail">${data.email}</span>
+                            </div>
+                            <div class="item-option">
+                                <img src="./assets/icons/ellipsis-solid.svg" width="15" height="15">
+                            </div>
+                        </div>`;
+        $('#main-item-container').append(element);
     }
 }
 $('#icon-close-new-item').click(function () {
-    $('#new-item-container').removeClass('show')
-    $('#new-item-container').addClass('hide')
-
+    $('#new-item-container').removeClass('show');
+    $('#new-item-container').addClass('hide');
 })
 
 
 
 $(document).ready(function () {
-    addItems(10)
-
-    $('.items-container').each(function () {
-        
-        $(this).hover(function () {
-            var $item = $(this);
-            $('.item-option img').each(function(){
-                $(this).hide();
-            })
-            $item.find('.item-option img').each(function () {
-                $(this).show()
-            })
-        })
-    })
+    GetAllPasswordItem();
 });
